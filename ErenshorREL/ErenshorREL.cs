cef4c595e7bac76bf9bf2c75df4c14c1a8b31914
Brumdail <brumdail@gmail.com>
2024-09-20 15:22:56 -0500

    bug fixes

--- ErenshorREL/ErenshorREL.cs
+++ ErenshorREL/ErenshorREL.cs
@@ -27,7 +27,7 @@ namespace ErenshorREL
     {
 
         internal const string ModName = "ErenshorREL";
-        internal const string ModVersion = "0.0.1";
+        internal const string ModVersion = "0.0.2";
         internal const string ModDescription = "Erenshor Random Epic Loot";
         internal const string Author = "Brumdail";
         private const string ModGUID = Author + "." + ModName;
@@ -61,6 +61,18 @@ namespace ErenshorREL
 
         private void Update()
         {
+            int levelsAboveHardcap = 4;
+            //check config values and limit the range
+            if (ErenshorREL.RandomLootLevelsBelow.Value < 0) { ErenshorREL.RandomLootLevelsBelow.Value = 0; }
+            if (ErenshorREL.RandomLootLevelsBelow.Value > 100) { ErenshorREL.RandomLootLevelsBelow.Value = 100; }
+            if (ErenshorREL.RandomLootLevelsAbove.Value < 0) { ErenshorREL.RandomLootLevelsAbove.Value = 0; }
+            //don't allow users to configure item levels above this additional level since they could get godly loot at level 1 otherwise.
+            if (ErenshorREL.RandomLootLevelsAbove.Value > levelsAboveHardcap) { ErenshorREL.RandomLootLevelsAbove.Value = levelsAboveHardcap; }
+            if (ErenshorREL.RandomLootCommonNPCChance.Value < 0.0f) { ErenshorREL.RandomLootCommonNPCChance.Value = 0.0f; }
+            if (ErenshorREL.RandomLootCommonNPCChance.Value > 100.0f) { ErenshorREL.RandomLootCommonNPCChance.Value = 100.0f; }
+            if (ErenshorREL.RandomLootRareNPCChance.Value < 0.0f) { ErenshorREL.RandomLootRareNPCChance.Value = 0.0f; }
+            if (ErenshorREL.RandomLootRareNPCChance.Value > 100.0f) { ErenshorREL.RandomLootRareNPCChance.Value = 100.0f; }
+            if ((ErenshorREL.RandomLootText.Value == "") || (ErenshorREL.RandomLootText.Value is null)) { ErenshorREL.RandomLootText.Value = "Mysterious forces have added a new treasure: "; }
             Config.Save();
         }
 
@@ -96,8 +108,12 @@ namespace ErenshorREL
         internal static ConfigEntry<Toggle> RandomLootToggle = null!;
         internal static ConfigEntry<int> RandomLootLevelsBelow = null!;
         internal static ConfigEntry<int> RandomLootLevelsAbove = null!;
-        internal static ConfigEntry<float> RandomLootChance = null!;
-        //internal static ConfigEntry<KeyboardShortcut> AutoRunKey = null!;
+        internal static ConfigEntry<float> RandomLootCommonNPCChance = null!;
+        internal static ConfigEntry<float> RandomLootRareNPCChance = null!;
+        internal static ConfigEntry<string> RandomLootText = null!;
+        //internal static ConfigEntry<Toggle> RandomLootRestrict = null!;
+        internal static ConfigEntry<Toggle> RandomLootDebug = null!;
+        //internal static ConfigEntry<KeyboardShortcut> RandomLootKey = null!;
         internal static bool _configApplied;
 
         internal ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description)
@@ -110,24 +126,6 @@ namespace ErenshorREL
         {
             return Config.Bind(group, name, value, new ConfigDescription(description));
         }
-
-        /*
-        internal ConfigEntry<T> TextEntryConfig<T>(string group, string name, T value, string desc)
-        {
-            ConfigurationManagerAttributes attributes = new()
-            {
-                CustomDrawer = Utilities.TextAreaDrawer
-            };
-            return Config.Bind(group, name, value, new ConfigDescription(desc, null, attributes));
-        }
-
-        private class ConfigurationManagerAttributes
-        {
-            [UsedImplicitly] public int? Order;
-            [UsedImplicitly] public bool? Browsable;
-            [UsedImplicitly] public string? Category;
-            [UsedImplicitly] public Action<ConfigEntryBase>? CustomDrawer;
-        }*/
 
         internal class AcceptableShortcuts : AcceptableValueBase // Used for KeyboardShortcut Configs 
         {
@@ -154,44 +152,186 @@ namespace ErenshorREL
             /// Create an array of lists of the items at each item level
             /// </summary>
             public static List<Item>[]? ItemsByLevel { get; private set; }
-            
+
             static void Postfix()
             {
-                bool itemListDebug = true;
-                int maxItemLevel = 100;
-                // Initialize the array of lists
-                ItemsByLevel = new List<Item>[maxItemLevel + 1];
-                for (int i = 0; i <= maxItemLevel; i++)
+                // Ensure the feature is enabled and it's not the Demo -- Brian @ Burgee does not want non-demo items to be available in the demo or he'll have to pull them from the database.
+                if ((ErenshorREL.RandomLootToggle.Value == Toggle.On) && !GameData.GM.DemoBuild)
                 {
-                    ItemsByLevel[i] = new List<Item>();
-                }
-                foreach (Item item in GameData.ItemDB.ItemDB)
-                {
-                    // Add item to the appropriate list based on its level
-                    if (item.ItemLevel >= 0 && item.ItemLevel <= maxItemLevel)
+                    int maxItemLevel = 100;
+                    // Initialize the array of lists
+                    ItemsByLevel = new List<Item>[maxItemLevel + 1];
+                    for (int i = 0; i <= maxItemLevel; i++)
                     {
-                        ItemsByLevel[item.ItemLevel].Add(item);
+                        ItemsByLevel[i] = new List<Item>();
+                    }
+                    foreach (Item item in GameData.ItemDB.ItemDB)
+                    {
+                        // Add item to the appropriate list based on its level
+                        if (item.ItemLevel >= 1 && item.ItemLevel <= maxItemLevel && item.ItemValue > 0)
+                        {
+                            ItemsByLevel[item.ItemLevel].Add(item);
+                        }
+                    }
+
+                    if (ErenshorREL.RandomLootDebug.Value == Toggle.On)
+                    {
+                        //Output the results:
+                        string filePath = System.IO.Path.Combine(Application.persistentDataPath, "ItemsByLevel.txt");
+                        using (StreamWriter writer = new StreamWriter(filePath))
+                        {
+                            for (int level = 1; level < ItemsByLevel.Length; level++)
+                            {
+                                List<Item> itemsAtLevel = ItemsByLevel[level];
+                                if (itemsAtLevel != null && itemsAtLevel.Count > 0)
+                                {
+                                    writer.WriteLine($"{itemsAtLevel.Count} Items at level {level}:");
+                                    foreach (Item item in itemsAtLevel)
+                                    {
+                                        string strClasses = "";
+                                        foreach (Class itemClass in item.Classes)
+                                        {
+                                            strClasses = strClasses + " " + itemClass.name;
+                                        }
+                                        writer.WriteLine($"- ID:{item.Id} {item.ItemName} Relic:{item.Relic} Unique:{item.Unique} Classes:{strClasses} Slot:{item.RequiredSlot.ToString()} Value:{item.ItemValue}");
+                                    }
+                                }
+                            }
+                        }
+                        Debug.Log($"ItemsByLevel written to: {filePath}");
                     }
                 }
+            }
+        }
 
-                if (itemListDebug)
+        [HarmonyPatch(typeof(Character))]
+        [HarmonyPatch("DoDeath")]
+        [HarmonyPriority(2)]
+        [HarmonyBefore("Brumdail.ErenshorQoL")]
+        class AddRandomLoot
+        {
+            /// <summary>
+            /// Attempts to add random loot to the latest nearby corpse after each Character.DoDeath call
+            /// </summary>
+            static void Postfix(Character __instance)
+            {
+                // Ensure the feature is enabled and it's not the Demo -- Brian @ Burgee does not want non-demo items to be available in the demo or he'll have to pull them from the database.
+                if ((ErenshorREL.RandomLootToggle.Value == Toggle.On) && !GameData.GM.DemoBuild)
                 {
-                    //DEBUG Output the results:
-                    string filePath = System.IO.Path.Combine(Application.persistentDataPath, "ItemsByLevel.txt");
-                    //string filePath = "C:\\Users\\brumd\\AppData\\LocalLow\\Burgee Media\\Erenshor\\ItemsByLevel.txt";
-                    using (StreamWriter writer = new StreamWriter(filePath))
+                    //Ensure the lists have been populated
+                    if (GenerateItemLists.ItemsByLevel is not null)
                     {
-                        for (int level = 0; level < ItemsByLevel.Length; level++)
+                        // Ensure the character is an NPC and has a valid level
+                        if (__instance.isNPC && __instance.MyStats.Level >= 1)
                         {
-                            List<Item> itemsAtLevel = ItemsByLevel[level];
-                            if (itemsAtLevel != null && itemsAtLevel.Count > 0)
+                            bool randomLootSuccess = false; //check for successful roll
+                            bool isNPCWithinDistance = false; //check for an NPC that died nearby the player
+                            bool isRareNPC = false; //needed to apply higher chance for a rare NPC
+                            float requiredChance = 0f;
+                            LootTable lootTable = __instance.GetComponent<LootTable>();
+                            float randomLootDistance = 60f;
+                            //int NPCGuaranteedDrops = 0;
+                            //int NPCCommonDrops = 0;
+                            //int NPCUncommonDrops = 0;
+                            //int NPCRareDrops = 0;
+                            //int NPCLegendaryDrops = 0;
+                            int NPCMaxItemDrops = 0;
+                            int NPCMaxNonCommonDrops = 0;
+                            int NPCActualDrops = 0;
+
+                            /*
+                            //moved this block to the update section:
+                            int levelsAboveHardcap = 4;
+                            //check config values and limit the range
+                            if (ErenshorREL.RandomLootLevelsBelow.Value < 0) { ErenshorREL.RandomLootLevelsBelow.Value = 0; }
+                            if (ErenshorREL.RandomLootLevelsBelow.Value > 100) { ErenshorREL.RandomLootLevelsBelow.Value = 100; }
+                            if (ErenshorREL.RandomLootLevelsAbove.Value < 0) { ErenshorREL.RandomLootLevelsAbove.Value = 0; }
+                            //don't allow users to configure item levels above this additional level since they could get godly loot at level 1 otherwise.
+                            if (ErenshorREL.RandomLootLevelsAbove.Value > levelsAboveHardcap) { ErenshorREL.RandomLootLevelsAbove.Value = levelsAboveHardcap; }
+                            if (ErenshorREL.RandomLootCommonNPCChance.Value < 0.0f) { ErenshorREL.RandomLootCommonNPCChance.Value = 0.0f; }
+                            if (ErenshorREL.RandomLootCommonNPCChance.Value > 100.0f) { ErenshorREL.RandomLootCommonNPCChance.Value = 100.0f; }
+                            if (ErenshorREL.RandomLootRareNPCChance.Value < 0.0f) { ErenshorREL.RandomLootRareNPCChance.Value = 0.0f; }
+                            if (ErenshorREL.RandomLootRareNPCChance.Value > 100.0f) { ErenshorREL.RandomLootRareNPCChance.Value = 100.0f; }
+                            if ((ErenshorREL.RandomLootText.Value == "") || (ErenshorREL.RandomLootText.Value is null)) { ErenshorREL.RandomLootText.Value = "Mysterious forces have added a new treasure: "; }
+                            */
+
+                            //Is the NPC rare?
+                            //check NPC loot table for how many rare items it can possibly drop
+                            //NPCGuaranteedDrops = lootTable.GuaranteeOneDrop.Count;
+                            //NPCCommonDrops = lootTable.CommonDrop.Count;
+                            //NPCUncommonDrops = lootTable.UncommonDrop.Count;
+                            //NPCRareDrops = lootTable.RareDrop.Count;
+                            //NPCLegendaryDrops = lootTable.LegendaryDrop.Count;
+                            /*if (ErenshorREL.RandomLootRestrict.Value == Toggle.On)
                             {
-                                //Debug.Log($"{itemsAtLevel.Count} Items at level {level}:");
-                                writer.WriteLine($"{itemsAtLevel.Count} Items at level {level}:");
-                                foreach (Item item in itemsAtLevel)
+                                //disqualify if no guaranteed drops
+                                if (NPCGuaranteedDrops <= 0) { NPCEligible = false; }
+                            }*/
+                            //new method -- based on number of drops:
+                            NPCMaxItemDrops = lootTable.MaxNumberDrops;
+                            NPCMaxNonCommonDrops = lootTable.MaxNonCommonDrops;
+                            NPCActualDrops = lootTable.ActualDrops.Count;
+                            if ((NPCMaxNonCommonDrops > 2) || (NPCActualDrops > 2)) { isRareNPC = true; }
+
+
+                            //check resulting level calculations
+                            int minLevel = __instance.MyStats.Level - ErenshorREL.RandomLootLevelsBelow.Value;
+                            if (minLevel <= 0) { minLevel = 1; }
+                            int maxLevel = __instance.MyStats.Level + ErenshorREL.RandomLootLevelsAbove.Value;
+                            if (maxLevel > 100) { maxLevel = 100; }
+                            if (maxLevel <= 0) { maxLevel = 1; }
+
+                            float NPCDistance = Vector3.Distance(GameData.PlayerControl.transform.position, __instance.transform.position);
+                            if (NPCDistance < randomLootDistance) { isNPCWithinDistance = true; }
+
+
+
+                            float randomRoll = UnityEngine.Random.Range(0f, 100f);
+                            if (isRareNPC)
+                            {
+                                requiredChance = ErenshorREL.RandomLootRareNPCChance.Value;
+                            }
+                            else
+                            {
+                                requiredChance = ErenshorREL.RandomLootCommonNPCChance.Value;
+                            }
+                            if (randomRoll <= requiredChance) { randomLootSuccess = true; }
+
+                            //determine possible loot list by using the level ranges
+                            List<Item> possibleLoot = new List<Item>();
+                            if (isNPCWithinDistance && randomLootSuccess)
+                            {
+
+                                for (int i = minLevel; i <= maxLevel; i++)
                                 {
-                                    //Debug.Log($"- ID:{item.Id} {item.ItemName}");
-                                    writer.WriteLine($"- ID:{item.Id} {item.ItemName} Relic:{item.Relic} Unique:{item.Unique} Classes:{item.Classes.ToString()} Slot:{item.RequiredSlot.ToString()} Value:{item.ItemValue}");
+                                    possibleLoot.AddRange(GenerateItemLists.ItemsByLevel[i]);
+                                }
+
+                                //= GenerateItemLists.ItemsByLevel[__instance.MyStats.Level];
+                                if (possibleLoot != null && possibleLoot.Count > 0)
+                                {
+                                    // Select a random item from the list
+                                    Item randomLoot = possibleLoot[UnityEngine.Random.Range(0, possibleLoot.Count)];
+
+                                    // Add the item to the drop table
+                                    lootTable.ActualDrops.Add(randomLoot);
+
+                                    // Log the loot addition for debugging purposes
+                                    if (ErenshorREL.RandomLootDebug.Value == Toggle.On) { UpdateSocialLog.LogAdd($"Generate Loot for {__instance.transform.name}: Level: {__instance.MyStats.Level} [{minLevel} to {maxLevel}] containing {possibleLoot.Count} items ", "yellow"); }
+                                    // Output to the player
+                                    UpdateSocialLog.LogAdd($"{ErenshorREL.RandomLootText.Value} + { randomLoot.ItemName} to { __instance.transform.name}", "green");
+                                }
+                            }
+                            else { //unsuccessful for one or more reasons
+                                if (ErenshorREL.RandomLootDebug.Value == Toggle.On) 
+                                {
+                                    string unsuccessfulReason = "";
+                                    int reasonsCount = 0;
+                                    if (!isNPCWithinDistance) {reasonsCount++; unsuccessfulReason = unsuccessfulReason + $"{reasonsCount}. NPC too far ({NPCDistance})."; }
+                                    //if (!NPCEligible) { unsuccessfulReason = unsuccessfulReason + $" NPC doesn't have enough rare loot normally (Guaranteed Drops:{NPCGuaranteedDrops}, Common:{NPCCommonDrops}, Uncommon:{NPCUncommonDrops}, Rare:{NPCRareDrops}, Legendary:{NPCLegendaryDrops})."; }
+                                    //if (!NPCEligible) { reasonsCount++; unsuccessfulReason = unsuccessfulReason + $"{reasonsCount}. NPC doesn't have Guaranteed Drops."; }
+                                    if (!randomLootSuccess) { reasonsCount++; unsuccessfulReason = unsuccessfulReason + $"{reasonsCount}. Roll of {randomRoll} does not fall within required chance {requiredChance} (Rare Assumed:{isRareNPC})"; }
+                                    UpdateSocialLog.LogAdd($"DO NOT Generate Loot because:{unsuccessfulReason}", "yellow"); 
                                 }
                             }
                         }
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ErenshorREL
-Erenshor Random Epic Loot Mod based on BepInEx.
+Erenshor Random Epic Loot Mod based on BepInEx
 
-## Features: Get similar level random loot from bosses, tweak loot tables and more!
+# Does not work in the Demo - Beta/Full version only!
+
+## Features: Get similar level random loot from enemies!
 
 `This mod uses a file watcher. If the configuration file is not changed with BepInEx Configuration manager, but changed in the file directly, upon file save, it will automatically apply the settings.`
 ---
@@ -12,23 +14,36 @@ You can adjust the config values in-game using the [Configuration Manager]https:
 
 
 ## How it works:
-- Get similar level random loot from bosses, tweak loot tables and more!
-- By default, adds 1 random piece of loot when killing a rare NPC that drops an uncommon drop or better
-- Loot is chosen from an item level range based on the defeated enemy's level
-- Because this mod requires editing the LootTable of NPCs, additional tweaks are available to increase or decrease drop percentages
+- Get similar level random loot from enemies!
+- Configure the chance that common and rare NPCs can drop extra random loot!
+- Loot is chosen from an item level range based on the defeated enemy's level.
 
+## How to Install (Beta or Full Erenshor License REQUIRED):
 
-## How to Install:
-
-### (Recommended for Beta Testers/full Erenshor License):
+### Recommended:
 Click the Install with Mod Manager button
 
-### Manual (Recommended for Demo Users):
+### Manual:
 1. Install BepInEx - https://thunderstore.io/c/erenshor/p/BepInEx/BepInExPack/.
 2. Download manually, extract the Brumdail-ErenshorREL folder and move into the Erenshor\BepInEx\plugins folder.
 
 ## Technical Details
 Adds Postfix commands to ItemDatabase.Start() to create lists of items per level
-Adds Prefix commands to TypeText.CheckCommands() to include new commands
+Adds Postfix commands to Character.DoDeath() to insert new random loot
+
+### Changelog:
+- 2024-09-20 - Fixed compatibility with other mods. Initial release.
 
 
+# Author Information
+
+### Brumdail
+
+`DISCORD:` Brumdail
+
+Special thanks to Mod config code from https://github.com/AzumattDev
+
+For Questions or Comments, find me in the Erenshor Discord:
+
+[![https://erenshor.com/gallery_gen/a5a278886e7e0bdedd386bd74929d9fa_fit.png](https://erenshor.com/gallery_gen/a5a278886e7e0bdedd386bd74929d9fa_fit.png)](https://discord.gg/DxqZc5Dc9q)
+<a href="https://discord.gg/DxqZc5Dc9q">
